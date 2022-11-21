93c2071176b6d73121378f2080adcc175b808049
Soumyadeep Basu <44787782+basusoumyadeep@users.noreply.github.com>
2022-11-21 19:00:57 +0100

    fix heatmap hover

--- res/simian_viewer/simian_viewer.js
+++ res/simian_viewer/simian_viewer.js
@@ -1164,15 +1164,15 @@ const simianVis = () => {
         svg.call(tip);
 
 
-    var dblclick  = function (d) {
-/*        d3.select("#marker1").remove();
+/*    var dblclick  = function (d) {
+*//*        d3.select("#marker1").remove();
         d3.select("#marker2").remove();
         d3.select("#marker3").remove();
-        d3.select("#exploreViewMarker").remove();*/
+        d3.select("#exploreViewMarker").remove();*//*
         d3.select("#exploreViewMarker1").remove();
         d3.select("#exploreViewMarker2").remove();
         d3.select("#exploreViewMarker3").remove();
-/*        if (barflag) {
+*//*        if (barflag) {
             svg.append("line")
                 .attr("id", "marker1")
                 .attr("x1", 0)
@@ -1248,14 +1248,14 @@ const simianVis = () => {
         selectionIDs.push(d.cross_species_cluster2_species_2);
         if (isQtAvailable) {
             QtBridge.js_passSelectionToQt(selectionIDs);
-        }*/
-    };
-    var mouseout = function (d) {
-/*        if (d3.event.shiftKey) {
+        }*//*
+    };*/
+/*    var mouseout = function (d) {
+*//*        if (d3.event.shiftKey) {
             if (showExplorationModeflag) { showExplorationModeflag = false; tip.show(d, d3.mouse(this), this); }
 
         }
-        if (!showExplorationModeflag)*/
+        if (!showExplorationModeflag)*//*
         if (d3.event.shiftKey) { }
     else
         {
@@ -1265,7 +1265,7 @@ const simianVis = () => {
             d3.select("#exploreViewMarker3").remove();
         }
 
-    };
+    };*/
 /*    var contextmenu = function (d) {
         tip.hide();
         d3.select("#exploreViewMarker1").remove();
@@ -1320,7 +1320,6 @@ const simianVis = () => {
             .style("cursor", "default");
 
 
-
     };
     var mousemove = function (d) {
         /*        document.getElementById("clearTooltip");
@@ -1368,26 +1367,102 @@ const simianVis = () => {
         rightBarGroupTooltip.selectAll('.bar.right').data(exampleDataTooltip).enter().append('rect').attr('class', 'bar right').attr('x', 0).attr('y', function (d) { return yScaleTooltip(d.group); }).attr('width', function (d) { return xScaleTooltip(d.species2); }).attr('fill', speciesColors[species2ValueIdentify]).attr('height', yScaleTooltip.bandwidth());
         function translation(x, y) { return 'translate(' + x + ',' + y + ')'; }
         }
+        if (d3.event.shiftKey) {
+            d3.select("#exploreViewMarker1").remove();
+            d3.select("#exploreViewMarker2").remove();
+            d3.select("#exploreViewMarker3").remove();
 
+
+            if (barflag) {
+                svg.append("line")
+                    .attr("id", "exploreViewMarker1")
+                    .attr("x1", 0)
+                    .attr("y1", d3.mouse(this)[1])
+                    .attr("x2", width)
+                    .attr("y2", d3.mouse(this)[1])
+                    .attr("stroke-width", 2)
+                    .style("stroke-dasharray", ("3, 3"))
+                    .style("stroke-opacity", 0.9)
+                    .attr("stroke", in_speciesClustercolors[d.cluster_2])
+                    .attr("marker-end", "url(#triangle)");
+
+                svg.append("line")
+                    .attr("id", "exploreViewMarker2")
+                    .attr("x1", d3.mouse(this)[0])
+                    .attr("y1", 0)
+                    .attr("x2", d3.mouse(this)[0])
+                    .attr("y2", height)
+                    .attr("stroke-width", 2)
+                    .style("stroke-dasharray", ("3, 3"))
+                    .style("stroke-opacity", 0.9)
+                    .attr("stroke", in_speciesClustercolors[d.cluster_1])
+                    .attr("marker-end", "url(#triangle)");
+
+                svg.append("circle")
+                    .attr("id", "exploreViewMarker3")
+                    .attr("cx", d3.mouse(this)[0])
+                    .attr("cy", d3.mouse(this)[1])
+                    .attr("r", 5)
+                    .style("fill", myColor(d.dist))
+                    .style("stroke", "black")
+                    .attr("stroke-width", 1);
+
+            }
+            else {
+
+                svg.append("line")
+                    .attr("id", "exploreViewMarker1")
+                    .attr("x1", 0)
+                    .attr("y1", d3.mouse(this)[1])
+                    .attr("x2", d3.mouse(this)[0])
+                    .attr("y2", d3.mouse(this)[1])
+                    .attr("stroke-width", 2)
+                    .style("stroke-dasharray", ("3, 3"))
+                    .style("stroke-opacity", 0.9)
+                    .attr("stroke", in_speciesClustercolors[d.cluster_2])
+                    .attr("marker-end", "url(#triangle)");
+
+                svg.append("line")
+                    .attr("id", "exploreViewMarker2")
+                    .attr("x1", d3.mouse(this)[0])
+                    .attr("y1", d3.mouse(this)[1])
+                    .attr("x2", d3.mouse(this)[0])
+                    .attr("y2", height)
+                    .attr("stroke-width", 2)
+                    .style("stroke-dasharray", ("3, 3"))
+                    .style("stroke-opacity", 0.9)
+                    .attr("stroke", in_speciesClustercolors[d.cluster_1])
+                    .attr("marker-end", "url(#triangle)");
+
+                svg.append("circle")
+                    .attr("id", "exploreViewMarker3")
+                    .attr("cx", d3.mouse(this)[0])
+                    .attr("cy", d3.mouse(this)[1])
+                    .attr("r", 5)
+                    .style("fill", myColor(d.dist))
+                    .style("stroke", "black")
+                    .attr("stroke-width", 1);
+            }
+        }
 
     };
     var mouseleave = function (d) {
 
-
+        d3.select(this).style("stroke", function (d) {
+            if (d.cross_species_cluster1_species_1 == d.cross_species_cluster2_species_2) {
+                return cross_speciesClustercolors[d.cross_species_cluster1_species_1];
+            }
+            else {
+                return "black";
+            }
+        })
+            .style("opacity", 0.9).style("stroke-width", function (d) {
+                return 0;
+            });
 
         if (d3.event.shiftKey) { }
         else {
-            d3.select(this).style("stroke", function (d) {
-                if (d.cross_species_cluster1_species_1 == d.cross_species_cluster2_species_2) {
-                    return cross_speciesClustercolors[d.cross_species_cluster1_species_1];
-                }
-                else {
-                    return "black";
-                }
-            })
-                .style("opacity", 0.9).style("stroke-width", function (d) {
-                    return 0;
-                });
+
 
             tip.hide();
             d3.select("#exploreViewMarker1").remove();
@@ -1559,7 +1634,7 @@ const simianVis = () => {
 
         })
         .style("opacity", 0.9)
-        .on("mouseover", mouseover)
+        .on("mouseenter", mouseover)
         //.on("mouseout", mouseout)
         .on("mousemove", mousemove)
         .on("mouseleave", mouseleave)
